33e848befcc7b5f627f2b92811a793f87ca00afa
troussvi <victor.troussel@gmail.com>
2019-01-31 23:32:09 +0100

    Quizz 1 + Quizz2 - Résultat + calcul score

--- press-pepper/public/js/nav.js
+++ press-pepper/public/js/nav.js
@@ -137,11 +137,69 @@ class ScrollPages {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-    var s = new ScrollPages(1,4,document.getElementById('all-pages'));
+	
+	var score=0;
+    var s = new ScrollPages(1,10,document.getElementById('all-pages'));
     s.init();
-		
+	
+	//Quizz 1 elem 1 -2 -3
 	i=document.getElementById("click");
-	i.onclick=function() { s.scrollTo(4); };
+	r=document.getElementById("clickT");
+	i.onclick=function() {i.style.borderRadius="50%";i.style.border = "thick solid red";setTimeout(function() {s.scrollTo(4);}, 700);};
+	i.onmouseover=function(){r.style.opacity="0.8";i.style.width="19%";};
+	i.onmouseout=function(){r.style.opacity="0.0";i.style.width="18%";};
+	
+	i1=document.getElementById("click2");
+	r2=document.getElementById("clickT2");
+	i1.onclick=function(){score++;i1.style.borderRadius="50%";i1.style.border = "thick solid green";	setTimeout(function() {s.scrollTo(4);}, 700);};
+	i1.onmouseover=function(){r2.style.opacity="0.8";i1.style.width="19%";};
+	i1.onmouseout=function(){r2.style.opacity="0.0";i1.style.width="18%";};
+	
+	r3=document.getElementById("clickT3");	
+	i2=document.getElementById("click3");
+	i2.onclick=function() {i2.style.borderRadius="50%";i2.style.border = "thick solid red";	setTimeout(function() {s.scrollTo(4);}, 700);};
+	i2.onmouseover=function(){r3.style.opacity="0.8";i2.style.width="19%";};
+	i2.onmouseout=function(){r3.style.opacity="0.0";i2.style.width="18%";};
+	
+	//quizz2 elem 1-2-3
+
+	ii=document.getElementById("clickk");
+	rr=document.getElementById("clickkT");
+	ii.onclick=function() {ii.style.borderRadius="50%";ii.style.border = "thick solid red";setTimeout(function() {s.scrollTo(6);}, 700);};
+	ii.onmouseover=function(){rr.style.opacity="0.8";ii.style.width="19%";};
+	ii.onmouseout=function(){rr.style.opacity="0.0";ii.style.width="18%";};
+	
+	ii1=document.getElementById("clickk2");
+	rr2=document.getElementById("clickkT2");
+	ii1.onclick=function(){score++;ii1.style.borderRadius="50%";ii1.style.border = "thick solid green";	setTimeout(function() {s.scrollTo(6);}, 700);};
+	ii1.onmouseover=function(){rr2.style.opacity="0.8";ii1.style.width="19%";};
+	ii1.onmouseout=function(){rr2.style.opacity="0.0";ii1.style.width="18%";};
+	
+	rr3=document.getElementById("clickkT3");	
+	ii2=document.getElementById("clickk3");
+	ii2.onclick=function() {ii2.style.borderRadius="50%";ii2.style.border = "thick solid red";	setTimeout(function() {s.scrollTo(6);}, 700);};
+	ii2.onmouseover=function(){rr3.style.opacity="0.8";ii2.style.width="19%";};
+	ii2.onmouseout=function(){rr3.style.opacity="0.0";ii2.style.width="18%";};
+	
+	//quizz3 elem 1-2-3
+
+	/*iii=document.getElementById("click");
+	rrr=document.getElementById("clickT");
+	iii.onclick=function() {iii.style.borderRadius="50%";iii.style.border = "thick solid red";setTimeout(function() {s.scrollTo(4);}, 700);};
+	iii.onmouseover=function(){rrr.style.opacity="0.8";iii.style.width="19%";};
+	iii.onmouseout=function(){rrr.style.opacity="0.0";iii.style.width="18%";};
+	
+	iii1=document.getElementById("click2");
+	rrr2=document.getElementById("clickT2");
+	iii1.onclick=function(){score++;iii1.style.borderRadius="50%";iii1.style.border = "thick solid green";	setTimeout(function() {s.scrollTo(4);}, 700);};
+	iii1.onmouseover=function(){rrr2.style.opacity="0.8";iii1.style.width="19%";};
+	iii1.onmouseout=function(){rrr2.style.opacity="0.0";iii1.style.width="18%";};
+	
+	rrr3=document.getElementById("clickT3");	
+	iii2=document.getElementById("click3");
+	iii2.onclick=function() {iii2.style.borderRadius="50%";iii2.style.border = "thick solid red";	setTimeout(function() {s.scrollTo(4);}, 700);};
+	iii2.onmouseover=function(){rrr3.style.opacity="0.8";iii2.style.width="19%";};
+	iii2.onmouseout=function(){rrr3.style.opacity="0.0";iii2.style.width="18%";};*/
 	
 	var delayInMilliseconds = 3000; 
 	setTimeout(function() {
