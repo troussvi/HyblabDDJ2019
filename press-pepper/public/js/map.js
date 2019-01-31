96fedb39ee3b1df55ab03858179e66e7b7445925
RaphaëlS <raphael.sanglier@etu.univ-nantes.fr>
2019-01-31 20:31:09 +0100

    ajout carte

--- press-pepper/public/js/map.js
+++ press-pepper/public/js/map.js
@@ -1,13 +1,7 @@
-var mymap = L.map('mapid',{attributionControl: false, zoomControl: false}).setView([47.895950, -1.750340], 7);
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
-	{foo: 'bar',
-	minZoom: 7,
-	maxZoom: 12
-	}).addTo(mymap);
-
-var circle = L.circle([47.775830, -1.346720], {
-	color: 'red',
-	fillColor: '#f03',
-	fillOpacity: 0.5,
-	radius: 500
-}).addTo(mymap);
+mapboxgl.accessToken = 'pk.eyJ1Ijoib3VtYXJtYXQiLCJhIjoiY2pyaHczd21wMWo3ZzRhcnp0cHRrcHFlZSJ9.cF3bbbmbxh7L2wC_nzKrSA';
+const map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/oumarmat/cjrjfzdj62lbs2snmdyerm9sd',
+center: [-1.277561, 48.197539],
+zoom: 7.3
+});
