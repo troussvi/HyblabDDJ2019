943b6f5b142d4a22e001968fd853cd75b26f7ec0
RaphaëlS <raphael.sanglier@etu.univ-nantes.fr>
2019-02-01 09:16:39 +0100

    LES POINTS

--- press-pepper/public/js/map.js
+++ press-pepper/public/js/map.js
@@ -1,7 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoib3VtYXJtYXQiLCJhIjoiY2pyaHczd21wMWo3ZzRhcnp0cHRrcHFlZSJ9.cF3bbbmbxh7L2wC_nzKrSA';
 const map = new mapboxgl.Map({
 container: 'map',
+attributionControl:false,
 style: 'mapbox://styles/oumarmat/cjrjfzdj62lbs2snmdyerm9sd',
 center: [-1.277561, 48.197539],
-zoom: 7.3
+zoom: 6
 });
+
