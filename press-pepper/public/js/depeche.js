d576467e556b9b2a0de6e8980b1c99296ba8e7fd
RaphaëlS <raphael.sanglier@etu.univ-nantes.fr>
2019-02-01 17:55:55 +0100

    FIN DE TOURNAGE POUR TODAY

---
+++ press-pepper/public/js/depeche.js
@@ -0,0 +1,12 @@
+function populate_html(ville,contentieux){
+	// afficher le nom de la ville, le nom du contentieux (balises strong?)
+	//get tous les id associés à ces critères
+	//supprimer doublons
+	//print les titres dans des balises <a></a> déclenchant depecheprint(idAssocie) 
+}
+function depeche_print(id){
+	// à partir de l'id envoyé, on remplit la div depeche
+}
+function depeche_cache(){
+	//il faut effacer le contenu et mettre en hidden (facile)
+}
